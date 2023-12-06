2bd2a6001faf595f4df0d0f029fba10cdaa582a5
FrankDevStar <reginafrankjames30@gmail.com>
2023-12-06 21:39:03 +0200

    update

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+const String appName = "zkZeroSea Wallet";
+const infuraKey = "f8ae20e8984c4329b41f09845248a63a";
+const aboutUrl = "https://zkzerosea.pro/security.html";
+const helpUrl = "https://zkzerosea.pro/help.html";
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,24 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_secure_storage_macos
+import mobile_scanner
+import package_info_plus_macos
+import path_provider_foundation
+import share_plus_macos
+import sqflite
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
